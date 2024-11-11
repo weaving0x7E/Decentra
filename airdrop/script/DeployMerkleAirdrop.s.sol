c9c3642d5d6c5775edfa4891052fc9dd2817419a
gaozilong <gaozilong@pinefield-inc.com>
2024-11-11 12:51:21 +0800

    [ADD] unit test for airdrop

---
+++ airdrop/script/DeployMerkleAirdrop.s.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.24;
+
+import {Script} from "forge-std/Script.sol";
+import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
+import {BagelToken} from "../src/BagelToken.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract DeployMerkleAirdrop is Script {
+    bytes32 private s_merkleRoot = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+    uint256 private s_amountToTransfer = 4 * 25e18;
+
+    function deployMerkleAirdrop() public returns (MerkleAirdrop, BagelToken) {
+        vm.startBroadcast();
+        BagelToken token = new BagelToken();
+        MerkleAirdrop airdrop = new MerkleAirdrop(s_merkleRoot, IERC20(address(token)));
+        token.mint(token.owner(), s_amountToTransfer);
+        token.transfer(address(airdrop), s_amountToTransfer);
+        vm.stopBroadcast();
+        return (airdrop, token);
+    }
+
+    function run() external returns (MerkleAirdrop, BagelToken) {
+        return deployMerkleAirdrop();
+    }
+}
---
+++ airdrop/test/MerkleAirdrop.t.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.24;
+
+import {Test, console} from "forge-std/Test.sol";
+import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
+import {BagelToken} from "../src/BagelToken.sol";
+
+contract MerkleAirDropTest is Test {
+    MerkleAirdrop public airdrop;
+    BagelToken public token;
+
+    bytes32 public ROOT = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+    uint256 public AMOUNT = 25e18;
+    bytes32 proofOne = 0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
+    bytes32 proofTwo = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
+    bytes32[] public PROOF = [proofOne, proofTwo];
+
+    address gasPayer;
+    address user;
+    uint256 userPrivateKey;
+
+    function setUp() public {
+        token = new BagelToken();
+        airdrop = new MerkleAirdrop(ROOT, token);
+        token.mint(token.owner(), AMOUNT * 8);
+        token.transfer(address(airdrop), AMOUNT * 8);
+        (user, userPrivateKey) = makeAddrAndKey("user");
+        gasPayer = makeAddr("gasPayer");
+    }
+
+    function testUserCanClaim() public {
+        uint256 initialBalance = token.balanceOf(user);
+        bytes32 digest = airdrop.getMessageHash(user, AMOUNT);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(userPrivateKey, digest);
+
+        vm.prank(gasPayer);
+        airdrop.claim(user, AMOUNT, PROOF, v, r, s);
+
+        uint256 endingBalance = token.balanceOf(user);
+        console.log("ending balance", endingBalance);
+        assertEq(endingBalance - initialBalance, AMOUNT);
+    }
+}
