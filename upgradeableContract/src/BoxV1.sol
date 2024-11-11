9c8a0cacf773869faa5622112c0cc7fc61cf0cf2
gaozilong <gaozilong@pinefield-inc.com>
2024-11-11 14:32:01 +0800

    [IMP] upgradeable contract

---
+++ upgradeableContract/src/BoxV1.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.18;
+
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract BoxV1 is Initializable, UUPSUpgradeable, OwnableUpgradeable {
+    uint256 internal number;
+
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialized(address owner) public initializer {
+        __Ownable_init(owner);
+        __UUPSUpgradeable_init();
+    }
+
+    function getNumber() external view returns (uint256) {
+        return number;
+    }
+
+    function version() external pure returns (uint256) {
+        return 1;
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override {}
+}
